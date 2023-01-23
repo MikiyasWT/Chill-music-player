98c2e0c4ea5ccf196b2db9052f0fa799c90179bf
MikiyasWT <mikiywendu44@gmail.com>
2023-01-23 10:38:01 +0300

    bugs fixed

--- src/components/SongsLibrary.js
+++ src/components/SongsLibrary.js
@@ -1,13 +1,64 @@
+// import { playAudio } from "../lib/util" 
+
+// const SongsLibrary = ({audioRef,song,songs,setSongs,setCurrentSong,isPlaying,setIsPlaying,id}) =>{
+
+   
+
+//     const songSelectHandler = async () => {
+        
+//         await setCurrentSong({...song,active:true})
+//         await setIsPlaying(true)
+//         const newSongs = songs.map((song) => {
+//             if(song.id === id){
+//                 return {
+//                     ...song,
+//                     active:true,   
+//                 }
+               
+             
+//             }
+//             else {
+//                 return {
+//                     ...song,
+//                     active:false,   
+//                 }
+//             }
+//         })
+
+//         setSongs(newSongs);
+//         audioRef.current.play();
+  
+        
+
+//     }
+
+   
+
+//     return (
+//         <div className={`library-song ${song.active ? "selected":"" } `} onClick={songSelectHandler}>
+//             <img src={song.cover} alt={song.name}></img>
+//             <div className="song-description">
+//             <h2>{song.name}</h2>
+//             <h3>{song.artist}</h3>
+//             </div>
+            
+//         </div>
+//     )
+// }
+
+
+// export default SongsLibrary;
+
+
 import { playAudio } from "../lib/util" 
 
 const SongsLibrary = ({audioRef,song,songs,setSongs,setCurrentSong,isPlaying,setIsPlaying,id}) =>{
 
    
-
-    const songSelectHandler = async () => {
+    const songSelectHandler = () => {
         
-        await setCurrentSong({...song,active:true})
-        await setIsPlaying(true)
+        setCurrentSong({...song,active:true})
+        
         const newSongs = songs.map((song) => {
             if(song.id === id){
                 return {
@@ -26,14 +77,22 @@ const SongsLibrary = ({audioRef,song,songs,setSongs,setCurrentSong,isPlaying,set
         })
 
         setSongs(newSongs);
-        audioRef.current.play();
-  
-        
+
+            const isPlayingPromise = audioRef.current.play();
+
+            if(isPlayingPromise !==undefined){
+              isPlayingPromise.then((audio) => {
+                audioRef.current.play()
+                setIsPlaying(true)
+              })
+
+            }
+        playAudio(audioRef,setIsPlaying)
+
+
 
     }
-
    
-
     return (
         <div className={`library-song ${song.active ? "selected":"" } `} onClick={songSelectHandler}>
             <img src={song.cover} alt={song.name}></img>
@@ -45,6 +104,4 @@ const SongsLibrary = ({audioRef,song,songs,setSongs,setCurrentSong,isPlaying,set
         </div>
     )
 }
-
-
 export default SongsLibrary;
--- src/lib/util.js
+++ src/lib/util.js
@@ -1,11 +1,12 @@
 
-export const playAudio = async (audioRef,setIsPlaying) => {
+export const playAudio = async (audioRef) => {
         
         const isPlayingPromise = await audioRef.current.play();
         if(isPlayingPromise !== undefined){
             isPlayingPromise.then((audio) => {
               audioRef.current.play()
-              setIsPlaying(true)
+              //setIsPlaying(!isPlaying)
+
             })
               
           
